829a62e09851352e10a6de18b5240d5986ab8c8d
EduardoCezarCarvalho <eduardo.carvalho1@estudante.ufla.br>
2022-09-24 02:36:47 -0300

    Criação do método MB3

--- B.cpp
+++ B.cpp
@@ -7,6 +7,7 @@ class B{
         ~B();
         void MB1();
         void MB2();
+        void MB3();
         int getB1();
         float getB2();
         void setB1(int B1);
@@ -21,6 +22,10 @@ void B::MB1(){ }
 
 void B::MB2(){ }
 
+void B::MB3(){
+    std::cout << "MB3" << std::endl;
+}
+
 int B::getB1(){ return B1; }
 
 float B::getB2(){ return B2; }
@@ -28,4 +33,3 @@ float B::getB2(){ return B2; }
 void B::setB1(int B1){ this->B1 = B1; }
 
 void B::setB2(float B2){ this->B2 = B2; }
-
