23a18f8ec9d54844e71606134897897e7d798d03
EduardoCezarCarvalho <eduardo.carvalho1@estudante.ufla.br>
2022-09-24 02:37:47 -0300

    Criação da classe D

---
+++ D.cpp
@@ -0,0 +1,15 @@
+class D{
+	private:
+        char D1;
+        unsigned D2;
+    public:
+        D();
+        ~D();
+        void MD1();
+        void MD2();
+        void MD3();
+};
+
+void D::MD1(){ }
+
+void D::MD2(){ }
